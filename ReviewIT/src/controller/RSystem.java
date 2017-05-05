f77703b93258cfc660063817ff90735f8ed0ed78
Dimitar Kumanov <dimitark@uw.edu>
2017-05-04 21:06:05 -0700

    Added more documentation and code safety

--- ReviewIT/src/controller/RSystem.java
+++ ReviewIT/src/controller/RSystem.java
@@ -40,16 +40,9 @@ public class RSystem {
 		deserializeData();
 	}
 	
-	private void deserializeData(){
-		/*
-		 * TO DO: Code for deserializing Data, aka loading up our
-		 * Conference/UserProfile objects should happen here
-		 */
-	}
-	
 	/**
-	 * Gets the only instance that exists for this class.
-	 * @return a RSystem Object which knows about our Conferences/UserProfiles/etc.
+	 * Gets THE RSystem instance guaranteed to be the only one in existence.
+	 * @return a RSystem Object which keeps track of our Conferences/UserProfiles/etc.
 	 * @author Dimitar Kumanov
 	 */
 	public static RSystem getInstance(){
@@ -59,8 +52,19 @@ public class RSystem {
 	}
 	
 	/**
+	 * Loads up all Conference/UserProfile objects into the RSYstem.
+	 */
+	private void deserializeData(){
+		/*
+		 * TO DO: Code for deserializing Data, aka loading up our
+		 * Conference/UserProfile objects should happen here
+		 */
+	}
+	
+	/**
+	 * Saves all state(data) into files for a later session of the program.
 	 * This method should be called before closing the
-	 * application to save all of its state(data) for future sessions.
+	 * application to save all of its Conference/UserProfile Objects.
 	 */
 	public static void serializeModel(){
 		/*
@@ -68,15 +72,34 @@ public class RSystem {
 		 * Conference/UserProfile objects should happen here
 		 */
 	}
+	
+	/**
+	 * Gets all of the Conferences in the system.
+	 * @return a (non-null) List of Conferences in the system.
+	 */
 	public List<Conference> getConferences(){
 		return new ArrayList<>(myConferenceMap.values());
 	}
 	
+	/**
+	 * Gets a specific Conference in system based on theConferenceName
+	 * @param theConferenceName the Conference name to match a Conference with
+	 * @return the specific Conference in system based on theConferenceName. null if no Conference matches.
+	 * @author Dimitar Kumanov
+	 */
 	public Conference getConference(final String theConferenceName){
 		return myConferenceMap.get(theConferenceName);
 	}
 	
-	public void addConference(final Conference theConference){
+	/**
+	 * Adds a Conference to the System.
+	 * @param theConference the Conference to add to the RSystem.
+	 * @throws IllegalArgumentException if theConference name matches a Conference already in the RSystem.
+	 */
+	public void addConference(final Conference theConference) throws IllegalArgumentException{
+		if(myConferenceMap.containsKey(theConference.getInfo().getName())){
+			throw new IllegalArgumentException("There exists a Conference with this name in the RSystem already!");
+		}
 		myConferenceMap.put(theConference.getInfo().getName(), theConference);
 	}
 	
@@ -90,7 +113,15 @@ public class RSystem {
 		return myUserMap.get(theUserID);
 	}
 	
-	public void addUserProfile(final UserProfile theUserProfile){
+	/**
+	 * Adds a UserProfile to the RSystem.
+	 * @param theUserProfile  the Conference to add to the RSystem.
+	 * @throws IllegalArgumentException  if theUserProfile's userID matches a UserProfile's userID already in the RSystem.
+	 */
+	public void addUserProfile(final UserProfile theUserProfile) throws IllegalArgumentException{
+		if(myUserMap.containsKey(theUserProfile.getName())){
+			throw new IllegalArgumentException("There exists a UserProfile with userID in the RSystem already!");
+		}
 		myUserMap.put(theUserProfile.getUID(), theUserProfile);
 	}
 	
