2b67c8263a16f02be6155637bc44d1eb09894e06
ijj <ijj@mac2.lan>
2017-05-21 20:08:47 -0700

    Added more elements to SubmitPaper screen.

--- src/view/SubmitPaper.java
+++ src/view/SubmitPaper.java
@@ -18,7 +18,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
+/**
+ * 
+ * @author Ian Jury
+ *
+ */
 public class SubmitPaper extends PanelCard {
 
     /** The name to lookup this panel in a panel changer. */
@@ -33,22 +37,32 @@ public class SubmitPaper extends PanelCard {
     /** */
     private String currentFilePath = "No file has been selected.";
     
-    JPanel infoPanel = new JPanel();
+    private JPanel infoPanel = new JPanel();
+    
+    private JPanel selectionPanel = new JPanel();
     
     private Dimension preferredDimension = new Dimension(500, 20);
     
     /** */
     private String currentPaperTitle = "No title has been entered.";
     
+    private JTextField paperTitleTextField = new JTextField("Enter the paper's title here...");
+    
     public SubmitPaper(PanelChanger p, UserContext context) {
         super(p, context);
         this.setLayout(new BorderLayout());
+    }
+    
+	@Override
+    public String getNameOfPanel() {
+        return PANEL_LOOKUP_NAME;
     }
 
     @Override
     public void updatePanel() {
     	this.removeAll();
     	infoPanel.removeAll();
+    	selectionPanel.removeAll();
     	
     	 Objects.requireNonNull(context.getCurrentConference());
          Objects.requireNonNull(context.getUser());
@@ -76,14 +90,100 @@ public class SubmitPaper extends PanelCard {
         confirmationPanel.add(submitButton);
 		return confirmationPanel;
 	}
+    
+    /**
+     * 
+     * @return
+     */
+    private JPanel getInfoPanel() {
+    	//JPanel infoPanel = new JPanel();
+    	infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+    	
+    	JLabel title = new JLabel("Current paper information: ");
+    	JLabel spacing = new JLabel(" ");
+    	JLabel paperFilePath = new JLabel("Current file path: " + currentFilePath);
+    	JLabel paperTitleLabel = new JLabel("Current title: " + currentPaperTitle);
+    	JLabel paperAuthorsLabel = new JLabel("Current authors: ");
+    	
+    	
+    	infoPanel.add(title);
+    	infoPanel.add(spacing);
+    	infoPanel.add(paperFilePath);
+    	infoPanel.add(paperTitleLabel);
+    	infoPanel.add(paperAuthorsLabel);
+    	return infoPanel;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    private JPanel getSelectionPanel() {
+    	//JPanel selectionPanel = new JPanel();
+    	JLabel title = new JLabel("Enter information about paper here");
+    	selectionPanel.setLayout(new BoxLayout(selectionPanel, BoxLayout.Y_AXIS));
+    	JLabel spacing = new JLabel(" ");
+    	
+    	
+    	 //file chooser
+        JButton fileChooserButton = new JButton("Select file to upload...");
+        final FileNameExtensionFilter fileTypeFilter = new FileNameExtensionFilter(
+                "docx, doc, and pdf", "docx", "doc", "pdf");
+            fileChooser.setFileFilter(fileTypeFilter);
+        fileChooserButton.addActionListener(new SelectFileAction());
+        
+        //text field
+        
+        paperTitleTextField.setMaximumSize(preferredDimension);
+        JButton paperTitleEnterButton = new JButton("Confirm current title");
+        paperTitleEnterButton.addActionListener(new titleEnterAction());
+        
+        
+        JTextField authorsTextField = new JTextField("Enter authors (change this...)");
+        JButton paperAuthorEnterButton = new JButton("Add author to list");
+        authorsTextField.setMaximumSize(preferredDimension);
+        
+        selectionPanel.add(title);
+        selectionPanel.add(spacing);
+        selectionPanel.add(fileChooserButton);
+        
+        selectionPanel.add(paperTitleTextField);
+        selectionPanel.add(paperTitleEnterButton);
+        selectionPanel.add(spacing);
+        
+        selectionPanel.add(authorsTextField);
+        selectionPanel.add(paperAuthorEnterButton);
+    	return selectionPanel;
+    }
+    
+    /**
+     * Error message that displays if file selection process goes wrong.
+     * @param theMessage
+     */
+    private void displayErrorMessage(final String theMessage) {
+        JOptionPane.showMessageDialog(this, theMessage, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    
+    private class titleEnterAction implements ActionListener {
 
-	@Override
-    public String getNameOfPanel() {
-        return PANEL_LOOKUP_NAME;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			currentPaperTitle = paperTitleTextField.getText();
+			//this is super weird-- need to change
+        	panelChanger.changeTo(ConferenceSelection.PANEL_LOOKUP_NAME);
+        	panelChanger.changeTo(SubmitPaper.PANEL_LOOKUP_NAME);
+			
+		}
+    	
     }
     
     
     
+    /**
+     * 
+     * @author ijj
+     *
+     */
     private class SelectFileAction implements ActionListener {
 
 		@Override
@@ -93,7 +193,9 @@ public class SubmitPaper extends PanelCard {
 	        	File fileOfPaper = fileChooser.getSelectedFile();
 	        	currentFilePath = fileOfPaper.getAbsolutePath();
 	        	infoPanel.repaint();
-	        	repaint();
+	        	//this is super weird-- need to change
+	        	panelChanger.changeTo(ConferenceSelection.PANEL_LOOKUP_NAME);
+	        	panelChanger.changeTo(SubmitPaper.PANEL_LOOKUP_NAME);
 	                      
 	        } else if (returnVal == JFileChooser.ERROR_OPTION) { 
 	            displayErrorMessage("There was an error loading the selected file!");
@@ -111,55 +213,6 @@ public class SubmitPaper extends PanelCard {
 			
 		}
     	
-    }
-    
-    private void displayErrorMessage(final String theMessage) {
-        JOptionPane.showMessageDialog(this, theMessage, "Error", JOptionPane.ERROR_MESSAGE);
-    }
-    
-    private JPanel getInfoPanel() {
-    	//JPanel infoPanel = new JPanel();
-    	infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
-    	
-    	JLabel title = new JLabel("Current paper information: ");
-    	JLabel spacing = new JLabel(" ");
-    	JLabel paperFilePath = new JLabel("Current file path: " + currentFilePath);
-    	
-    	
-    	infoPanel.add(title);
-    	infoPanel.add(spacing);
-    	infoPanel.add(paperFilePath);
-    	return infoPanel;
-    }
-    
-    private JPanel getSelectionPanel() {
-    	JPanel selectionPanel = new JPanel();
-    	JLabel title = new JLabel("Enter information about paper here");
-    	selectionPanel.setLayout(new BoxLayout(selectionPanel, BoxLayout.Y_AXIS));
-    	JLabel spacing = new JLabel(" ");
-    	
-    	
-    	 //file chooser
-        JButton fileChooserButton = new JButton("Select file to upload...");
-        final FileNameExtensionFilter fileTypeFilter = new FileNameExtensionFilter(
-                "docx, doc, and pdf", "docx", "doc", "pdf");
-            fileChooser.setFileFilter(fileTypeFilter);
-        fileChooserButton.addActionListener(new SelectFileAction());
-        
-        //text field
-        JTextField paperTitleTextField = new JTextField("Enter the paper's title here...");
-        paperTitleTextField.setMaximumSize(preferredDimension);
-        
-        
-        JTextField authorsTextField = new JTextField("Enter authors (change this...)");
-        authorsTextField.setMaximumSize(preferredDimension);
-        
-        selectionPanel.add(title);
-        selectionPanel.add(spacing);
-        selectionPanel.add(fileChooserButton);
-        selectionPanel.add(paperTitleTextField);
-        selectionPanel.add(authorsTextField);
-    	return selectionPanel;
     }
 
 }
